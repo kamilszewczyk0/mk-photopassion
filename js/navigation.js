bf6adeaf5bd979faf421190d9b7c1b207345c830
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-06-27 00:42:22 +0200

    added responsive menu for all cards

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+const burgerMenu = document.querySelector(".navigation__burger");
+const menu = document.querySelector(".navigation__menu");
+const body = document.querySelector("body");
+
+burgerMenu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  burgerMenu.classList.toggle("burger-open");
+});
