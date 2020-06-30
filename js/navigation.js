6aa23ee94292c13585aa5b2deee2effde3982974
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-06-30 23:57:39 +0200

    added hover effect

--- js/navigation.js
+++ js/navigation.js
@@ -4,5 +4,5 @@ const body = document.querySelector("body");
 
 burgerMenu.addEventListener("click", () => {
   menu.classList.toggle("open");
-  burgerMenu.classList.toggle("burger-open");
+  burgerMenu.classList.toggle("burger--open");
 });
